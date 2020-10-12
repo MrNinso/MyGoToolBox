594eebe5e0ebb2aeefd3fdbb4b3f1b42f380cb4b
developer <jheison540as@gmail.com>
2020-10-12 11:42:53 -0300

    Add env tools

---
+++ lang/env/env.go
@@ -0,0 +1,25 @@
+package env
+
+import (
+	. "github.com/MrNinso/MyGoToolBox/lang/ifs"
+	"os"
+	"strconv"
+)
+
+func GetEnv(key, defaultValue string) string {
+	return IfStringEmpty(os.Getenv(key), defaultValue)
+}
+
+func SetEnv(key, value string) (hasUpdated bool, err error) {
+	_, exists := os.LookupEnv(key)
+	return exists, os.Setenv(key, value)
+}
+
+func GetIntFromEnv(key string, defaulValue string) (int, error) {
+	v := GetEnv(key, "")
+
+	i, err := strconv.Atoi(v)
+
+	return IfReturn(err == nil, i, defaulValue).(int), err
+
+}
